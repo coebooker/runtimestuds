674d995e673052cc64ed9a75c8e6d621f6ea2243
coebooker <43794479+coebooker@users.noreply.github.com>
2020-09-30 15:20:31 -0500

    Add files via upload
    Added Course Database

---
+++ CourseDatabase/CourseDatabase/CourseHistory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CourseHistory]
+(
+	[username] TEXT NOT NULL PRIMARY KEY, 
+    [course_name] TEXT NOT NULL, 
+    [term] TEXT NOT NULL, 
+    [course-credit] TEXT NOT NULL, 
+    [grade] TEXT NOT NULL
+)
