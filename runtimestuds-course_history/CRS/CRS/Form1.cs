27097af8332feaab2847b13fd778d8d5063ab68d
sk1103 <60674142+sk1103@users.noreply.github.com>
2020-10-08 17:30:04 -0500

    Add files via upload
    Added:
    Add course functionality for students
    Add course functionality for admins
    Decrementing seats available upon adding courses
    Checking for invalid course adding. -> Maybe some bugs/errors left.
    
    Things left to do:
    Delete course feature & Incrementing seats with deletion (required)
    Brush-up aesthetics (optional)
    Add view current schedule feature for students (optional)
    
    Things I wasn't sure how to fix:
    Renewing the tables when a course is added to change seats available
    It does this when courses are added via student account but not when added via admin account. Couldn't figure out cause.

---
+++ runtimestuds-course_history/CRS/CRS/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRS
+{
+    public partial class LoginForm : Form
+    {
+        private userDatabase usrDB;
+        string usrf_path;
+        string crsf_path;
+        string prevcrsf_path;
+        public LoginForm(string upath, string cpath, string pcpath)
+        {
+            InitializeComponent();
+            usrf_path = upath;
+            crsf_path = cpath;
+            prevcrsf_path = pcpath;
+
+            userDatabase userDB = new userDatabase(usrf_path);
+            usrDB = userDB;
+            Bitmap bmp = ((Bitmap)close.BackgroundImage);
+            bmp.MakeTransparent();
+        }
+
+        private void UsernameEnter(object sender, EventArgs e)
+        {
+            if (username.Text == "Username")
+            {
+                username.Text = "";
+                username.ForeColor = Color.White;
+            }
+        }
+
+        private void UsernameLeave(object sender, EventArgs e)
+        {
+            if (username.Text == "")
+            {
+                username.Text = "Username";
+                username.ForeColor = Color.Silver;
+            }
+        }
+
+        private void PasswordEnter(object sender, EventArgs e)
+        {
+            if (password.Text == "Password")
+            {
+                password.Text = "";
+                password.PasswordChar = '*';
+                password.ForeColor = Color.White;
+            }
+        }
+
+        private void PasswordLeave(object sender, EventArgs e)
+        {
+            if (password.Text == "")
+            {
+                password.Text = "Password";
+                password.PasswordChar = '\0';
+                password.ForeColor = Color.Silver;
+            }
+        }
+
+        private void LoginClick(object sender, EventArgs e)
+        {
+            string usertype = "student";    //Default
+            string username = this.username.Text.ToLower();
+            string pswd = password.Text.ToLower();
+
+            if (usrDB.isValidUser(username, pswd, ref usertype))
+            {
+                this.Hide();
+                new mainpage(usertype, username, usrDB, usrf_path, crsf_path, prevcrsf_path).ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Your username or password is incorrect.",
+                    "Invalid Credential",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Question);
+                password.Text = "";
+                password.PasswordChar = '*';
+                password.ForeColor = Color.White;
+            }
+        }
+
+        private void close_click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ runtimestuds-course_history/CRS/CRS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRS
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string userfile_path = @"C:\Users\81802\Downloads\userDB.in";
+            string coursefile_path = @"C:\Users\81802\Downloads\courseDB.in";
+            string prevcoursefile_path = @"C:\Users\81802\Downloads\historyDB.in";
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm(userfile_path, coursefile_path, prevcoursefile_path));
+        }
+    }
+}
