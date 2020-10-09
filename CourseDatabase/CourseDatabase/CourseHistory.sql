c39929e1f51f2e4fee2cfee6aa5ab911af8ffaa1
CoenTM <mikitakamaru@gmail.com>
2020-10-08 21:43:16 -0500

    Added view current schedule feature for a student.

--- CourseDatabase/CourseDatabase/CourseHistory.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[CourseHistory]
-(
-	[username] TEXT NOT NULL PRIMARY KEY, 
-    [course_name] TEXT NOT NULL, 
-    [term] TEXT NOT NULL, 
-    [course-credit] TEXT NOT NULL, 
-    [grade] TEXT NOT NULL
-)
